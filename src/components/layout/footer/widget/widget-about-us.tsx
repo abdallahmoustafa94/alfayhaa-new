526d6ea3f2e29983a42f3afdbb9f05a9b65adefc
abdallahmoustafa94 <dimo.am@gmail.com>
2023-10-08 17:30:35 +0300

    edit: footer about text

--- src/components/layout/footer/widget/widget-about-us.tsx
+++ src/components/layout/footer/widget/widget-about-us.tsx
@@ -23,11 +23,13 @@ const WidgetAbout: React.FC<AboutProps> = ({ social, className }) => {
     <div className={`pb-10 sm:pb-0 ${className}`}>
       <div className="flex flex-col text-center sm:text-start max-w-[300px] mx-auto sm:ms-0 pb-6 sm:pb-5">
         <Logo href={ROUTES.HOME} className="mb-3 lg:mb-5 mx-auto sm:ms-0" />
-        <Text>{t('text-about-us')}</Text>
       </div>
 
       {social && (
-        <ul className="flex flex-wrap justify-center sm:justify-start space-s-4 md:space-s-5 mx-auto md:mx-0">
+        <ul
+          className="flex flex-wrap justify-center space-s-4 md:space-s-5 "
+          style={{ justifyContent: 'center' }}
+        >
           {social?.map((item) => (
             <li
               className="transition hover:opacity-80"
@@ -40,7 +42,8 @@ const WidgetAbout: React.FC<AboutProps> = ({ social, className }) => {
                     alt={item.name}
                     height={item.height}
                     width={item.width}
-                    className="transform scale-85 md:scale-100"
+                    className="transform scale-85 md:scale-100 "
+                    style={{ justifyContent: 'center' }}
                   />
                 </a>
               </Link>
--- src/components/layout/footer/widget/widget.tsx
+++ src/components/layout/footer/widget/widget.tsx
@@ -25,27 +25,21 @@ const Widgets: React.FC<WidgetsProps> = ({ widgets }) => {
           <WidgetLink
             key={`footer-widget--key${widget.id}`}
             data={widget}
-            className="pb-3.5 sm:pb-0 col-span-4 md:col-span-4 text-center"
+            className="pb-3.5 sm:pb-0 col-span-4 md:col-span-4 text-center m-auto"
           />
         ))}
-        <div className='col-span-4 text-center'>
-        <h3 className="text-skin-base text-base text-center lg:text-[17px] lg:leading-7 font-medium mb-4 sm:mb-5 lg:mb-6 pb-0.5">Contact us</h3>
-        <div>
-<a href="tel:01093942104">
-01093942104
-</a>   
-- <a href="tel:01016257613">
-01016257613
-</a>
-<br />
-<a href="tel:01114545799">
-01114545799
-</a> - <a href="tel:01112073657">
-01112073657
-</a> - <a href="tel:01026060560">
-01026060560
-</a>
-</div>
+        <div className="col-span-4 text-center">
+          <h3 className="text-skin-base text-base text-center lg:text-[17px] lg:leading-7 font-medium mb-4 sm:mb-5 lg:mb-6 pb-0.5">
+            Contact us
+          </h3>
+          <div>
+            <a href="tel:01093942104">01093942104</a>-{' '}
+            <a href="tel:01016257613">01016257613</a>
+            <br />
+            <a href="tel:01114545799">01114545799</a> -{' '}
+            <a href="tel:01112073657">01112073657</a> -{' '}
+            <a href="tel:01026060560">01026060560</a>
+          </div>
         </div>
         {/* <WidgetSubscription className="col-span-full sm:col-span-1 md:col-start-4 xl:col-start-auto md:col-span-4 xl:col-span-3 2xl:ps-7 3xl:ps-16 pt-8 sm:pt-0 border-t sm:border-t-0 border-skin-three " /> */}
       </div>
